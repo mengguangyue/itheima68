57805a4a54a29c4ddbbf649088e0c0a179453ffe
mgy <mgy@Mgy>
2017-12-06 23:01:55 +0800

    e3-common

---
+++ e3-common/src/main/java/cn/e3/utils/DatagridPagebean.java
@@ -0,0 +1,33 @@
+package cn.e3.utils;
+
+import java.io.Serializable;
+import java.util.List;
+
+//组装分页JavaBean对象--返回值
+public class DatagridPagebean implements Serializable {
+
+	//总记录数
+	private Long total;
+	
+	//总记录
+	private List<?> rows;
+
+	public Long getTotal() {
+		return total;
+	}
+
+	public void setTotal(Long total) {
+		this.total = total;
+	}
+
+	public List<?> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<?> rows) {
+		this.rows = rows;
+	}
+	
+	
+	
+}
